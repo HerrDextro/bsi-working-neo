2dc77f98ff503c9b1cb75c8acef8de91497a8c0a
Keinstein <alex@see5.ch>
2026-03-27 15:55:09 +0100

    created backend in c#

---
+++ Backend/CommBackend/CommBackend/Endpoints/UserEndpoints.cs
@@ -0,0 +1,13 @@
+﻿namespace CommBackend.Endpoints
+{
+    public static class UserEndpoints
+    {
+        public static void MapUserEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/Auth", () =>
+            {
+                return Results.Ok("Test");
+            });
+        }
+    }
+}
