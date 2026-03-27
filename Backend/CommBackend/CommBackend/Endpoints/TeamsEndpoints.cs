c89adfdf2ad3845dac28e7153204e9babbcdea86
Keinstein <alex@see5.ch>
2026-03-27 23:27:35 +0100

    added endpoints

--- Backend/CommBackend/CommBackend/Endpoints/TeamsEndpoints.cs
+++ Backend/CommBackend/CommBackend/Endpoints/TeamsEndpoints.cs
@@ -1,5 +1,6 @@
 ﻿using CommBackend.Models.Context;
 using CommBackend.Models.Data;
+using CommBackend.Models.Presentation.Auth;
 using CommBackend.Models.Presentation.Teams;
 using Microsoft.AspNetCore.Http.HttpResults;
 
@@ -18,7 +19,21 @@ namespace CommBackend.Endpoints
                 return Results.Ok(new TeamsResponse(teamsCall));
             });
 
-            //app.MapGet("/teams", ())
+            app.MapGet("/teams", async (CommContext db, bool activeOnly) =>
+            {
+                IQueryable<TeamsCall> calls = db.TeamsCalls;
+                DateTime now = DateTime.Now;
+                if (activeOnly)
+                {
+                    calls.Where(c => c.StartTime < now && now < c.EndTime);
+                }
+
+                List<TeamsCall> teamsCalls = calls.ToList();
+
+                List<TeamsResponse> Users = calls.Select(call => new TeamsResponse(call)).ToList();
+
+                return Users;
+            });
         }
     }
 }
